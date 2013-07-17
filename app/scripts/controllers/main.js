d7e6c4993081f10253614d856b6f705789470ec6
Joshua Petitt <joshpetitt@jpmec.com>
2013-07-17 10:50:44 -0500

    Initial files generated by Yeomen.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('doneApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
