518af49683d31e2d24642383fbef5a814c76ba3a
Joshua Petitt <joshpetitt@jpmec.com>
2013-07-28 22:50:35 -0500

    Improvements to help.  Added helpService.  Now allow help links to be included in other help pages.

--- app/help/help.js
+++ app/help/help.js
@@ -4,37 +4,55 @@
 
 
 angular.module('helpModule', ['userModule', 'todosModule', 'ui.bootstrap'])
+  .service('helpService', ['$dialog', '$location', function($dialog, $location) {
+
+    this.show = function(topic) {
+
+      if (!topic) {
+        var path = $location.path();
+        topic = path.substr(1, path.length);
+      }
+
+      var opts = {
+        backdrop: true,
+        keyboard: true,
+        backdropClick: true,
+        templateUrl: 'help/help_' + topic + '.html',
+        controller: 'helpDialogCtrl'
+      };
+
+      this.dialog = $dialog.dialog(opts);
+      this.dialog.open().then(function(result){
+          // do nothing
+        });
+    };
+  }])
+
   .directive('helpNavbar', function () {
     return {
       restrict: 'A',
       templateUrl: 'help/help_navbar.html'
     };
   })
-  .controller('helpCtrl', ['$scope', '$dialog', function($scope, $dialog){
+
+  .controller('helpCtrl', ['$scope', 'helpService', function($scope, helpService){
     $scope.init = function() {
     }
 
-    $scope.showHelp = function() {
-
-      $scope.opts = {
-        backdrop: true,
-        keyboard: true,
-        backdropClick: true,
-        templateUrl: 'help/help_about.html',
-        controller: 'helpDialogCtrl'
-      };
-
-      var d = $dialog.dialog($scope.opts);
-      d.open().then(function(result){
-          // do nothing
-        });
+    $scope.showHelp = function(topic) {
+      helpService.show(topic);
     };
   }])
 
 
 // Helper controller to close the help dialog
-function helpDialogCtrl($scope, dialog){
+function helpDialogCtrl($scope, dialog, helpService){
   $scope.close = function(result) {
     dialog.close(result);
+  };
+
+  $scope.showHelp = function(topic) {
+    dialog.close(null);
+    helpService.show(topic);
   };
 }
